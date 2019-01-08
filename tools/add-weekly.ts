3e1b48a3f7ea4325b95f7392f2fd1fbfaf9ae71c
fengshangwuqi <fengshangwuqi@gmail.com>
2019-01-08 15:30:49 +0800

    perf: rename getWeeklyMeta

--- tools/add-weekly.ts
+++ tools/add-weekly.ts
@@ -3,13 +3,17 @@ import { outputFile } from "fs-extra";
 
 import * as log from "./utils/log";
 import { paths } from "./utils/paths";
-import { getLtsWeeklyNum, getWeeklyMeta, getWeeklyName } from "./utils/weekly";
+import {
+  getLtsWeeklyNum,
+  getNewWeeklyMeta,
+  getWeeklyName
+} from "./utils/weekly";
 
 (async () => {
   const { title, author } = Config.get("weekly");
   const ltsWeeklyNum = getLtsWeeklyNum();
   const weeklyNum = ltsWeeklyNum + 1;
-  const meta = getWeeklyMeta({ title, author, weeklyNum });
+  const meta = getNewWeeklyMeta({ title, author, weeklyNum });
   const folderName = getWeeklyName(weeklyNum);
 
   await outputFile(`${paths.weeklyDir}/${folderName}`, meta);
--- tools/utils/weekly.ts
+++ tools/utils/weekly.ts
@@ -9,23 +9,6 @@ interface IWeeklyItem {
   link: string;
 }
 
-export const getWeeklyMeta = ({
-  title,
-  author,
-  weeklyNum
-}: {
-  title: string;
-  author: string;
-  weeklyNum: number;
-}) => {
-  return `---
-title: ${title} 第 ${weeklyNum} 期
-author: ${author}
-tags: 
-date: 
----`;
-};
-
 export const getLtsWeeklyNum = (): number => {
   const ltsWeeklyName = readdirSync(paths.weeklyDir).pop();
 
@@ -77,6 +60,23 @@ export const newWeeklyPrompt = async () => {
       name: "link"
     }
   ]);
+};
+
+export const getNewWeeklyMeta = ({
+  title,
+  author,
+  weeklyNum
+}: {
+  title: string;
+  author: string;
+  weeklyNum: number;
+}) => {
+  return `---
+title: ${title} 第 ${weeklyNum} 期
+author: ${author}
+tags: 
+date: 
+---`;
 };
 
 export const getNewWeeklyText = (
